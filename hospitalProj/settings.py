c4b355359a10f760fc6a56e277eee246b6a90a66
Joseph-k-iype <josephkiype@gmail.com>
2022-12-20 09:30:34 +0530

    abc

--- hospitalProj/settings.py
+++ hospitalProj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hospitalApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'hospitalProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,12 +79,11 @@ WSGI_APPLICATION = 'hospitalProj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'coviddb',
+        'NAME': 'hospitaldb',
         'USER': 'admin',
         'PASSWORD': '123ubuntu17.04LTS',
-        'HOST': 'covid19.c0rwgmkwfrp0.us-east-1.rds.amazonaws.com',
+        'HOST': 'hospital-mysql-server.cf3okaieihsj.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
-
     }
 }
 
